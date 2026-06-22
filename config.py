4bf3c827982b9994388719ed6add129955b37425
Nikhilkumar3065 <obulshettynikhilkumar@gmail.com>
2026-06-22 11:36:09 +0530

    Clean project structure and remove unnecessary files

---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py
+# ------------------------------------------
+# Stores all configuration settings
+# ------------------------------------------
+
+SECRET_KEY = "nikhilkumar"
+
+# MySQL Database
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "KNikhil@3065"
+DB_NAME = "smartcart_db"
+
+
+# Email SMTP Settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = 'nikhilkumar3065@gmail.com'
+MAIL_PASSWORD = 'loxm zsrj ihys gyyp'   # Gmail App Password
+MAIL_DEFAULT_SENDER = 'SmartCart <nikhilkumar3065@gmail.com>'  # Use the authenticated sender address with display name
+
+# razor pay
+RAZORPAY_KEY_ID = "rzp_test_T0fzGrjrzJrIvB"
+RAZORPAY_KEY_SECRET = "9Lii542McynpB4f4u2cV5zew"
